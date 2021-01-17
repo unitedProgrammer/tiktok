4a8f70d45fdebc3a4ebe102518ebb297d559c1ef
unitedProgrammer <77531232+unitedProgrammer@users.noreply.github.com>
2021-01-17 12:11:08 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import './App.css';
-import Video from './Video';
+import React from "react";
+import "./App.css";
+import Video from "./Video";
 
 function App() {
   return (
-    <div className="app"> 
-    <div className="app_videos">
-      <Video />
-      <Video />
-    </div>
-    
+    <div className="app">
+      <div className="app_videos">
+        <Video
+          url="Videos/6911735648202411270.mp4"
+          channel="dima"
+          description="wooooow dex"
+          song="charlie.wav"
+          likes={123}
+          messages={456}
+          shares={999}
+        />
+        <Video
+          url="Videos/6911735648202411270.mp4"
+          channel="dima"
+          description="wooooow dex"
+          song="charlie.wav"
+          likes={123}
+          messages={456}
+          shares={999}
+        />
+      </div>
     </div>
   );
 }
--- src/Video.js
+++ src/Video.js
@@ -1,35 +1,34 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from "react";
 import "./Video.css";
-import VideoFooter from "./VideoFooter"
-import VideoSideBar from './VideoSideBar';
+import VideoFooter from "./VideoFooter";
+import VideoSideBar from "./VideoSideBar";
 
-function Video() {
-    const [playing, setPlaying] = useState(false);
-    const videoRef = useRef(null);
-    const handleVideoPress = () => {
-if(playing){
-    videoRef.current.pause();
-    setPlaying(false);
-}
-else{
-    videoRef.current.play();
-    setPlaying(true);
-}
-
+function Video({ url, channel, description, shares, song, messages, likes }) {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+  const handleVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
     }
-    return (
-        <div className="video">
-            <video 
-            onClick={handleVideoPress}
-            className='video__player'
-            loop
-            ref={videoRef}>
-            <source src="/Videos/6911735648202411270.mp4" type="video/mp4"/>
-            </video>
-            <VideoFooter/>
-            <VideoSideBar />
-        </div>
-    );
+  };
+  return (
+    <div className="video">
+      <video
+        onClick={handleVideoPress}
+        className="video__player"
+        loop
+        ref={videoRef}
+      >
+        <source src={url} type="video/mp4" />
+      </video>
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSideBar likes={likes} shares={shares} messages={messages} />
+    </div>
+  );
 }
 
 export default Video;
