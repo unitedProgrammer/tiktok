afcd9372ab6168a0e0b2de15de3dbd3a198a2779
unitedProgrammer <77531232+unitedProgrammer@users.noreply.github.com>
2021-02-03 18:58:34 +0200

    fix999

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const data = [
   {
-    url: "Videos/6911735648202411270.mp4",
+    url: "Videos/8213.mp4",
     channel: "dima",
     description: "wooooow dex",
     song: "charlie.wav",
@@ -12,19 +12,28 @@ const data = [
     url: "Videos/tiktokvideo_1611844138.mp4",
     channel: "jacob",
     description: "und jallen wir",
-    song: "make sense.wav",
+    song: "drive.wav",
     likes: "122344",
     messages: "34",
     shares: "9949",
   },
   {
     url: "Videos/6911735648202411270.mp4",
-    channel: "jacob",
-    description: "und jallen wir",
-    song: "make sense.wav",
-    likes: "122343",
-    messages: "34",
-    shares: "9949",
+    channel: "john",
+    description: "love dance",
+    song: "u cant take me.wav",
+    likes: "0",
+    messages: "1245",
+    shares: "1324",
+  },
+  {
+    url: "Videos/tiktokvideo_1611844020.mp4",
+    channel: "mike",
+    description: "myvideo",
+    song: "crazy.wav",
+    likes: "9999999",
+    messages: "390",
+    shares: "10000",
   },
 ];
 export default data;
